d4f1e1bcc33d98d9a6f8f2baa209de2ebb66add1
igo312 <794330684@qq.com>
2022-08-22 14:42:32 +0800

    Update index.js
    change `wx.getUserInfo` to  `wx.getUserProfile`

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -23,7 +23,7 @@ Page({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+          wx.getUserProfile({
             success: res => {
               this.setData({
                 avatarUrl: res.userInfo.avatarUrl,
